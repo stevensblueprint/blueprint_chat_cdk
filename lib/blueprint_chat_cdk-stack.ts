d9c41ec3ef1f52e8aa10b5e782791e7413a64bd7
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2025-09-09 17:16:53 -0400

    feat: port stack from bedrock_gateway repo

--- lib/blueprint_chat_cdk-stack.ts
+++ lib/blueprint_chat_cdk-stack.ts
@@ -1,10 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as events from "aws-cdk-lib/aws-events";
 import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as path from "path";
 import { Construct } from "constructs";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class BlueprintChatCdkStack extends cdk.Stack {
   public readonly monthlyUsageTable: dynamodb.Table;
@@ -32,7 +34,7 @@ export class BlueprintChatCdkStack extends cdk.Stack {
         removalPolicy: cdk.RemovalPolicy.RETAIN,
         pointInTimeRecovery: true,
         encryption: dynamodb.TableEncryption.AWS_MANAGED,
-      },
+      }
     );
 
     this.transactionsTable = new dynamodb.Table(
@@ -52,7 +54,7 @@ export class BlueprintChatCdkStack extends cdk.Stack {
         removalPolicy: cdk.RemovalPolicy.RETAIN,
         pointInTimeRecovery: true,
         encryption: dynamodb.TableEncryption.AWS_MANAGED,
-      },
+      }
     );
 
     this.transactionsTable.addGlobalSecondaryIndex({
@@ -88,12 +90,7 @@ export class BlueprintChatCdkStack extends cdk.Stack {
         runtime: lambda.Runtime.PYTHON_3_10,
         handler: "main.handler",
         code: lambda.Code.fromAsset(
-          path.join(
-            __dirname,
-            "..",
-            "functions",
-            "inference-authorizer-lambda",
-          ),
+          path.join(__dirname, "..", "functions", "inference-authorizer-lambda")
         ),
         timeout: cdk.Duration.seconds(30),
         memorySize: 512,
@@ -101,8 +98,100 @@ export class BlueprintChatCdkStack extends cdk.Stack {
           MONTHLY_USAGE_TABLE: this.monthlyUsageTable.tableName,
           TRANSACTIONS_TABLE: this.transactionsTable.tableName,
         },
-      },
+      }
     );
+
+    const proxyFn = new lambdaNode.NodejsFunction(this, "BedrockProxyFn", {
+      entry: path.join(__dirname, "..", "functions", "inference-proxy-lambda"),
+      handler: "main.handler",
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(60),
+      environment: {
+        GLOBAL_MAX_TOKENS_PER_CALL: "1024",
+        REGION: this.region,
+      },
+    });
+
+    const api = new apigw.RestApi(this, "BedApiGatewayApi", {
+      restApiName: "bedrock-gateway-api",
+      description: "API Gateway for Bedrock proxy Lambda function",
+      deployOptions: {
+        stageName: "prod",
+        throttlingRateLimit: 20,
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: ["POST", "OPTIONS"],
+        allowHeaders: [
+          "Content-Type",
+          "Authorization",
+          "x-api-key",
+          "Accept",
+          "Origin",
+          "X-Requested-With",
+        ],
+        allowCredentials: false,
+      },
+      minCompressionSize: cdk.Size.bytes(1024),
+    });
+    api.addGatewayResponse("Default4xx", {
+      type: apigw.ResponseType.DEFAULT_4XX,
+      responseHeaders: {
+        "Access-Control-Allow-Origin": "'*'",
+        "Access-Control-Allow-Headers":
+          "'Content-Type,Authorization,x-api-key,Accept,Origin,X-Requested-With'",
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
+      },
+    });
+
+    api.addGatewayResponse("Default5xx", {
+      type: apigw.ResponseType.DEFAULT_5XX,
+      responseHeaders: {
+        "Access-Control-Allow-Origin": "'*'",
+        "Access-Control-Allow-Headers":
+          "'Content-Type,Authorization,x-api-key,Accept,Origin,X-Requested-With'",
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
+      },
+    });
+
+    const lambdaIntegration = new apigw.LambdaIntegration(proxyFn, {
+      proxy: true,
+      allowTestInvoke: true,
+    });
+
+    const v1 = api.root.addResource("v1");
+    const chat = v1.addResource("chat");
+
+    chat.addMethod("POST", lambdaIntegration, {
+      apiKeyRequired: false,
+    });
+
+    new cdk.CfnOutput(this, "LambdaFunctionName", {
+      value: proxyFn.functionName,
+      description:
+        "The name of the Lambda function that serves as the Bedrock proxy.",
+      exportName: "BedrockProxyFunctionName",
+    });
+
+    new cdk.CfnOutput(this, "LambdaFunctionArn", {
+      value: proxyFn.functionArn,
+      description:
+        "The ARN of the Lambda function that serves as the Bedrock proxy.",
+      exportName: "BedrockProxyFunctionArn",
+    });
+
+    new cdk.CfnOutput(this, "ApiInvokeUrl", {
+      value: `${api.url}v1/chat`,
+      description: "POST here to call the proxy.",
+      exportName: "BedrockGatewayInvokeUrl",
+    });
+
+    new cdk.CfnOutput(this, "Region", {
+      value: this.region,
+      description: "AWS Region where the stack is deployed",
+      exportName: "BedrockGatewayRegion",
+    });
 
     this.monthlyUsageTable.grantReadWriteData(inferenceAuthorizerFn);
     this.transactionsTable.grantReadWriteData(inferenceAuthorizerFn);
