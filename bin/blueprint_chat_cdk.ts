ce767451cad96cc4c38c2d581942ac33f4f0e586
miguel-merlin <mmerlin@stevens.edu>
2026-01-25 18:10:42 -0500

    feat: added env variables loading to cdk app

--- bin/blueprint_chat_cdk.ts
+++ bin/blueprint_chat_cdk.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import * as dotenv from "dotenv";
 import { BlueprintChatCdkStack } from "../lib/blueprint_chat_cdk-stack";
 
+dotenv.config();
+
 const app = new cdk.App();
-new BlueprintChatCdkStack(app, "BlueprintChatCdkStack", {});
+new BlueprintChatCdkStack(app, "BlueprintChatCdkStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
