2783d9a76d5be8ad9f445290c52c79096202b22a
miguel-merlin <mmerlin@stevens.edu>
2026-01-26 11:40:13 -0500

    feat: adeded webhook lambda construct

--- lib/blueprint_chat_cdk-stack.ts
+++ lib/blueprint_chat_cdk-stack.ts
@@ -1,13 +1,70 @@
+import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import LambdaLlmProxyConstruct from "./constructs/lambda_llm_proxy_construct";
+import WebhookLambdaConstruct from "./constructs/webhook_lamda_construct";
 
+export interface BlueprintChatCdkStackProps extends cdk.StackProps {
+  NOTION_API_KEY: string;
+  DISCORD_API_KEY: string;
+  DRIVE_API_KEY: string;
+  WIKI_API_KEY: string;
+}
 export class BlueprintChatCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: BlueprintChatCdkStackProps) {
     super(scope, id, props);
+
+    const documentBucket = new s3.Bucket(this, "DocumentBucket", {
+      bucketName: `${cdk.Stack.of(this).account.toLowerCase()}-blueprint-chat-documents`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
 
     new LambdaLlmProxyConstruct(this, "LambdaLlmProxy", {
       monthlyLimit: 6.6,
+    });
+
+    // Notion
+    new WebhookLambdaConstruct(this, "WebhookLambda", {
+      codePath: "functions/webhook-listener-notion-lambda",
+      description:
+        "Lambda function to handle Notion webhooks and store documents in S3",
+      documentBucket: documentBucket,
+      environmentVariables: {
+        NOTION_API_KEY: props.NOTION_API_KEY,
+      },
+    });
+
+    // Discord
+    new WebhookLambdaConstruct(this, "DiscordWebhookLambda", {
+      codePath: "functions/webhook-listener-discord-lambda",
+      description:
+        "Lambda function to handle Discord webhooks and store documents in S3",
+      documentBucket: documentBucket,
+      environmentVariables: {
+        DISCORD_API_KEY: props.DISCORD_API_KEY,
+      },
+    });
+
+    // Google Drive
+    new WebhookLambdaConstruct(this, "DriveWebhookLambda", {
+      codePath: "functions/webhook-listener-drive-lambda",
+      description:
+        "Lambda function to handle Google Drive webhooks and store documents in S3",
+      documentBucket: documentBucket,
+      environmentVariables: {
+        DRIVE_API_KEY: props.DRIVE_API_KEY,
+      },
+    });
+
+    // Wiki
+    new WebhookLambdaConstruct(this, "WikiWebhookLambda", {
+      codePath: "functions/webhook-listener-wiki-lambda",
+      description:
+        "Lambda function to handle Wiki webhooks and store documents in S3",
+      documentBucket: documentBucket,
+      environmentVariables: {
+        WIKI_API_KEY: props.WIKI_API_KEY,
+      },
     });
   }
 }
---
+++ lib/constructs/webhook_lamda_construct.ts
@@ -0,0 +1,71 @@
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cdk from "aws-cdk-lib";
+import * as path from "path";
+import { Construct } from "constructs";
+
+export interface WebhookLambdaConstructProps {
+  /**
+   * Code Path
+   */
+  codePath: string;
+
+  /**
+   * Description
+   */
+  description: string;
+
+  /**
+   * Document Bucket
+   * /
+   */
+  documentBucket: s3.IBucket;
+
+  /**
+   * Environment Variables
+   */
+  environmentVariables: { [key: string]: string };
+}
+
+export default class WebhookLambdaConstruct extends Construct {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: WebhookLambdaConstructProps,
+  ) {
+    super(scope, id);
+    const webhookListenerFn = new lambda.Function(this, "WebhookListenerFn", {
+      runtime: lambda.Runtime.PYTHON_3_10,
+      handler: "main.handler",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "..", "..", props.codePath),
+      ),
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 512,
+      environment: {
+        ...props.environmentVariables,
+        DOCUMENT_BUCKET: props.documentBucket.bucketName,
+      },
+      description: props.description,
+    });
+
+    props.documentBucket.grantReadWrite(webhookListenerFn);
+
+    const fnUrl = webhookListenerFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.POST],
+      },
+    });
+
+    new cdk.CfnOutput(this, "WebhookListenerFnArn", {
+      value: webhookListenerFn.functionArn,
+      description: "ARN of the Webhook Listener Lambda Function",
+    });
+    new cdk.CfnOutput(this, "WebhookListenerFnUrl", {
+      value: fnUrl.url,
+      description: "URL of the Webhook Listener Lambda Function",
+    });
+  }
+}
