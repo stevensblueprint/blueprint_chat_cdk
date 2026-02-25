9c3940207cdc01c3cbd96d48f4755cdc56402ce0
Pranav Goyal <63987638+PranavGoyal01@users.noreply.github.com>
2026-02-25 14:28:49 -0500

    feat: add webhook events queue to all webhook lambda constructs, extra formatting done by prettier

--- lib/stacks/blueprint_chat_cdk-stack.ts
+++ lib/stacks/blueprint_chat_cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import LambdaLlmProxyConstruct from "../constructs/lambda_llm_proxy_construct";
@@ -14,57 +15,61 @@ export class BlueprintChatCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: BlueprintChatCdkStackProps) {
     super(scope, id, props);
 
-    const documentBucket = new s3.Bucket(this, "DocumentBucket", {
-      bucketName: `${cdk.Stack.of(this).account.toLowerCase()}-blueprint-chat-documents`,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+		const documentBucket = new s3.Bucket(this, "DocumentBucket", {
+			bucketName: `${cdk.Stack.of(this).account.toLowerCase()}-blueprint-chat-documents`,
+			removalPolicy: cdk.RemovalPolicy.RETAIN,
+		});
+
+    const webhookEventsQueue = new sqs.Queue(this, "WebhookEventsQueue", {
+      visibilityTimeout: cdk.Duration.seconds(30),
     });
 
     new LambdaLlmProxyConstruct(this, "LambdaLlmProxy", {
       monthlyLimit: 6.6,
     });
 
-    // Notion
-    new WebhookLambdaConstruct(this, "NotionWebhookLambda", {
-      codePath: "functions/webhook-listener-notion-lambda",
-      description:
-        "Lambda function to handle Notion webhooks and store documents in S3",
-      documentBucket: documentBucket,
-      environmentVariables: {
-        NOTION_API_KEY: props.NOTION_API_KEY,
-      },
-    });
+		// Notion
+		new WebhookLambdaConstruct(this, "NotionWebhookLambda", {
+			codePath: "functions/webhook-listener-notion-lambda",
+			description: "Lambda function to handle Notion webhooks and store documents in S3",
+			documentBucket: documentBucket,
+			environmentVariables: {
+				NOTION_API_KEY: props.NOTION_API_KEY,
+			},
+			webhookEventsQueue,
+		});
 
-    // Discord
-    new WebhookLambdaConstruct(this, "DiscordWebhookLambda", {
-      codePath: "functions/webhook-listener-discord-lambda",
-      description:
-        "Lambda function to handle Discord webhooks and store documents in S3",
-      documentBucket: documentBucket,
-      environmentVariables: {
-        DISCORD_API_KEY: props.DISCORD_API_KEY,
-      },
-    });
+		// Discord
+		new WebhookLambdaConstruct(this, "DiscordWebhookLambda", {
+			codePath: "functions/webhook-listener-discord-lambda",
+			description: "Lambda function to handle Discord webhooks and store documents in S3",
+			documentBucket: documentBucket,
+			environmentVariables: {
+				DISCORD_API_KEY: props.DISCORD_API_KEY,
+			},
+			webhookEventsQueue,
+		});
 
-    // Google Drive
-    new WebhookLambdaConstruct(this, "DriveWebhookLambda", {
-      codePath: "functions/webhook-listener-drive-lambda",
-      description:
-        "Lambda function to handle Google Drive webhooks and store documents in S3",
-      documentBucket: documentBucket,
-      environmentVariables: {
-        DRIVE_API_KEY: props.DRIVE_API_KEY,
-      },
-    });
+		// Google Drive
+		new WebhookLambdaConstruct(this, "DriveWebhookLambda", {
+			codePath: "functions/webhook-listener-drive-lambda",
+			description: "Lambda function to handle Google Drive webhooks and store documents in S3",
+			documentBucket: documentBucket,
+			environmentVariables: {
+				DRIVE_API_KEY: props.DRIVE_API_KEY,
+			},
+			webhookEventsQueue,
+		});
 
-    // Wiki
-    new WebhookLambdaConstruct(this, "WikiWebhookLambda", {
-      codePath: "functions/webhook-listener-wiki-lambda",
-      description:
-        "Lambda function to handle Wiki webhooks and store documents in S3",
-      documentBucket: documentBucket,
-      environmentVariables: {
-        WIKI_API_KEY: props.WIKI_API_KEY,
-      },
-    });
-  }
+		// Wiki
+		new WebhookLambdaConstruct(this, "WikiWebhookLambda", {
+			codePath: "functions/webhook-listener-wiki-lambda",
+			description: "Lambda function to handle Wiki webhooks and store documents in S3",
+			documentBucket: documentBucket,
+			environmentVariables: {
+				WIKI_API_KEY: props.WIKI_API_KEY,
+			},
+			webhookEventsQueue,
+		});
+	}
 }
